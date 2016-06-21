167afec3255202da4a2cc1e1e76efa4c80a99317
andrewgy8 <andrewgy8@gmail.com>
2016-06-20 22:38:44 -0400

    Added a class buyer and test for the class.

--- lib/NegotiGate.rb
+++ lib/NegotiGate.rb
@@ -9,13 +9,47 @@ module NegotiGate
       @buyers_original_offer = buyers_original_offer
     end
 
+    # if the prices are equal, then the deal is finished
     def both_prices_equal?
-      if @sellers_original_price == @buyers_original_offer
+      @sellers_original_price == @buyers_original_offer
+    end
+
+    #if 'yes', then on to the negotiation engine
+    def negotiate?
+      if negotiate_answer == 'yes'
         true
-      else
-        false
       end
     end
 
+    #compares the two offers two each other
+    def compare_the_offers
+      if @sellers_original_price < @buyers_original_offer
+
+      elsif @sellers_original_price > @buyers_original_offer
+
+      else
+
+      end
+    
+    end
+  end
+
+  class User
+    attr_accessor :negotiate_answer, :user_name
+
+    def initialize(user_name)
+      @user_name = user_name
+    end
+
+    def negotiate_answer
+      @negotiate_answer = gets.chomp.downcase
+      if @negotiate_answer == 'yes'
+        true
+      end
+    end
   end
 end
+
+
+
+
--- spec/NegotiGate_spec.rb
+++ spec/NegotiGate_spec.rb
@@ -6,9 +6,7 @@ describe NegotiGate::Negotiation do
   describe ".both_prices_equal?" do
    
     context "given the sellers price is the same as the buyers highest buying price" do
-
       let(:negotiation) { described_class.new(10, 10) }
-
 
       it 'returns true' do
         expect(negotiation.both_prices_equal?).to be true
@@ -22,9 +20,17 @@ describe NegotiGate::Negotiation do
       it 'returns false' do
         expect(negotiation.both_prices_equal?).to be false
       end
-    end  
+    end
   end
-
- 
-
+  describe ".negotiate?" do
+    let(:negotiation) { described_class.new(10, 10)}
+    
+    context "given the seller and buyer would both like to negotiate" do
+      it 'returns true when yes' do
+        user1 = class_double("User")
+        user1.to receive(:answer).with('yes').and_return(:true)
+        expect(negotiation.negotiate?).to be true
+      end
+    end 
+  end
 end
